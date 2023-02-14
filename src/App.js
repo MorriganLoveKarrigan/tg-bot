7e537e085d89b6a1c92796188f7d1675dc1c68b1
m.ergashev <ergashevmaksud9@gmail.com>
2023-02-14 14:59:45 +0500

    useTelegram hook fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+
 
 function App() {
     const {tg,onToggleButton} = useTelegram();
@@ -16,6 +18,7 @@ function App() {
   return (
     <div className="App">
       WORKING
+        <Header/>
         <button onClick={onToggleButton}>toggle</button>
     </div>
   );
